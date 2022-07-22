baa98154f7ca038bb5215e1e58977c8fafd61242
Ana Bucur <ana.bucur@endava.com>
2022-07-22 19:13:19 +0300

    [JIRA-7] finish task received

--- math.c
+++ math.c
@@ -4,4 +4,15 @@ return a+b;
 
 int multiply(int a, int b){
 return a*b;
+<<<<<<< Updated upstream
 }
+=======
+}
+
+/* Use this method to divide 2 int numbers*/
+double division(int a, int b){
+return a-b;
+}
+
+something new added here
+>>>>>>> Stashed changes
